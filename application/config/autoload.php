99078cbea00d5446366e3d05addc854987afed91
Mithila <mithilamadusanka97@gmail.com>
2020-10-10 18:52:48 +0530

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'form_validation', 'session','forms');
+$autoload['libraries'] = array('database', 'form_validation', 'session',);
 
 /*
 | -------------------------------------------------------------------
