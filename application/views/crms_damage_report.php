c43ee0cb8f656337a6726cde07c53a24a254a302
Mithila <mithilamadusanka97@gmail.com>
2020-10-10 11:28:50 +0530

    Update crms_damageu_report.php
    updated

--- application/views/crms_damage_report.php
+++ application/views/crms_damage_report.php
@@ -25,7 +25,59 @@
         </div>
 
         <div class="row">
+            <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title" style="color: #F7396F;">Damage Details</h4>
+                        <!--<p class="card-description"> Add class <code>.table</code></p>-->
+                    
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Vehicle ID</th>
+                                    <th>Date</th>
+                                    <th>Discription</th>
+                                    <th>Reserved ID</th>
+                                    <th style="text-align: center;">Picture</th>
+                                    <th>Fix Amount</th>
+                                    <th>Is Solved</th>
+                                    <th style="text-align: center;">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                          	        <td>Ruvindu</td>
+                          	        <td>12 May 2020</td>
+                          	        <td>left or Right Signal light</td>
+                          	        <td>53275531</td>
+                          	        <td><input type="submit" name="" class="btn btn-success" value="View"></td>
+                          	        <td>1000 LKR/-</td>
+                          	        <td><b style="color: red;">Not yet</b></td>
+                          	        <td>
+                          		        <input type="submit" name="" class="btn btn-gradient-primary" value="Edit">
+                          		        <input type="submit" name="" class="btn btn-danger" value="Remove"></td>
+                          	        </td>
 
+                                </tr>
+                       	        <tr>
+                          	        <td>Madushan</td>
+                          	        <td>12 May 2020</td>
+                          	        <td>left or Right Signal light</td>
+                          	        <td>53275531</td>
+                          	        <td><input type="submit" name="" class="btn btn-success" value="View"></td>
+                          	        <td>1000 LKR/-</td>
+                          	        <td><b style="color: green;">Yes</b></td>
+                          	        <td>
+                          		        <input type="submit" name="" class="btn btn-gradient-primary" value="Edit">
+                          		        <input type="submit" name="" class="btn btn-danger" value="Remove"></td>
+                          	        </td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
