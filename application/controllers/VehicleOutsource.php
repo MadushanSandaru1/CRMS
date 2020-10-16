d7a33698e26d9c13d280e6c2b3c0924a14660813
Mithila <mithilamadusanka97@gmail.com>
2020-10-16 20:15:49 +0530

    created Vehicle outsourcing page

---
+++ application/controllers/VehicleOutsource.php
@@ -0,0 +1,27 @@
+<?php
+
+    class VehicleOutsource extends CI_Controller
+    {
+        public function  outsourcingVehicle()
+        {
+            $config['allowed_types'] = 'jpg|png|jpeg';
+            $config['upload_path'] = './assets/images/outsourceVehicles/';
+            $this->load->library('upload',$config);
+
+            if($this->upload->do_upload('outsource_pic'))
+            {
+                $data = $this->input->post();
+                $info = $this->upload->data();
+                $image_path= "assets/images/outsourceVehicles/".$info['raw_name'].$info['file_ext'];
+                $this->load->model('OutsourceVehicleModel');
+                $response = $this->OutsourceVehicleModel->insertOutsourceVehicle($image_path);
+
+                if ($response)
+                {
+                    $this->session->set_flashdata('outsource_status',"Data Recorded Successfully");
+                    $this->load->view('crms_outsourcing');
+                }
+            }
+        }
+    }
+?>
