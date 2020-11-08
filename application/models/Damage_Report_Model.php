8f20fa252168a2f5c002c197fb4f3645d1fb2816
Mithila <mithilamadusanka97@gmail.com>
2020-11-08 10:28:07 +0530

    updated

--- application/models/Damage_Report_Model.php
+++ application/models/Damage_Report_Model.php
@@ -41,86 +41,102 @@
                         }
             }
 
-            public function getDamages()
-            {
-                $start_date="";
-                $end_date="";
-                $vehicle_id =$this->input->post('vehicle_id',TRUE);
-                $get_time = $this->input->post('get_time',TRUE);
-                $damage_pic =$this->input->post('is_include_damage_picture',TRUE);
-                $solved_type =$this->input->post('is_solved_type',TRUE);
+            // public function getDamages()
+            // {
+            //     $start_date="";
+            //     $end_date="";
+            //     $vehicle_id =$this->input->post('vehicle_id',TRUE);
+            //     $get_time = $this->input->post('get_time',TRUE);
+            //     $damage_pic =$this->input->post('is_include_damage_picture',TRUE);
+            //     $solved_type =$this->input->post('is_solved_type',TRUE);
 
-                //conditions
-                if($get_time == "all")
-                {
-                        if($damage_pic == "No")
-                        {
-                                if($solved_type == "all")
-                                {
-                                    echo "time-all pic-no is_solved-all";
-                                }
-                                if($solved_type == "solved")
-                                {
-                                    echo "time-all pic-no is_solved-yes";
-                                }
-                                if($solved_type == "not_solved")
-                                {
-                                    echo "time-all pic-no is_solved-no";
-                                }
-                        }
-                        else
-                        {
-                                if($solved_type == "all")
-                                {
-                                    //echo "time-all pic-yes is_solved-all";
-                                    $this->db->where('vehicle_id',$vehicle_id);
-                                    $query = $this->db->get('damage');
-                                    return $query->result();
+            //     //conditions
+            //     if($get_time == "all")
+            //     {
+            //             if($damage_pic == "No")
+            //             {
+            //                     if($solved_type == "all")
+            //                     {
+            //                         echo "time-all pic-no is_solved-all";
+            //                     }
+            //                     if($solved_type == "solved")
+            //                     {
+            //                         echo "time-all pic-no is_solved-yes";
+            //                     }
+            //                     if($solved_type == "not_solved")
+            //                     {
+            //                         echo "time-all pic-no is_solved-no";
+            //                     }
+            //             }
+            //             else
+            //             {
+            //                     if($solved_type == "all")
+            //                     {
+            //                         //echo "time-all pic-yes is_solved-all";
+            //                         $this->db->where('vehicle_id',$vehicle_id);
+            //                         $query = $this->db->get('damage');
+            //                         return $query->result();
                                     
-                                }
-                                if($solved_type == "solved")
-                                {
-                                    echo "time-all pic-yes is_solved-yes";
-                                }
-                                if($solved_type == "not_solved")
-                                {
-                                    echo "time-all pic-yes is_solved-no";
-                                }
-                        }
+            //                     }
+            //                     if($solved_type == "solved")
+            //                     {
+            //                         echo "time-all pic-yes is_solved-yes";
+            //                     }
+            //                     if($solved_type == "not_solved")
+            //                     {
+            //                         echo "time-all pic-yes is_solved-no";
+            //                     }
+            //             }
+            //     }
+            //     else
+            //     {
+            //             if($damage_pic == "No")
+            //             {
+            //                     if($solved_type == "all")
+            //                     {
+            //                         echo "time-custom pic-no is_solved-all";
+            //                     }
+            //                     if($solved_type == "solved")
+            //                     {
+            //                         echo "time-custom pic-no is_solved-yes";
+            //                     }
+            //                     if($solved_type == "not_solved")
+            //                     {
+            //                         echo "time-custom pic-no is_solved-no";
+            //                     }
+            //             }
+            //             else
+            //             {
+            //                     if($solved_type == "all")
+            //                     {
+            //                         echo "time-custom pic-yes is_solved-all";
+            //                     }
+            //                     if($solved_type == "solved")
+            //                     {
+            //                         echo "time-custom pic-yes is_solved-yes";
+            //                     }
+            //                     if($solved_type == "not_solved")
+            //                     {
+            //                         echo "time-custom pic-yes is_solved-no";
+            //                     }
+            //             }
+            //     }
+            // }
+
+            public function getDateSpecificDamages()
+            {
+                $start_date=$this->input->post('start_date',TRUE);
+                $end_date=$this->input->post('end_date',TRUE);
+
+                $this->db->query("select *from damage where d_date >= '$start_date' ");
+                $query = $this->db->get('damage');
+                if($query->num_rows() > 0)
+                {
+                        return $query->result();
                 }
                 else
-                {
-                        if($damage_pic == "No")
-                        {
-                                if($solved_type == "all")
-                                {
-                                    echo "time-custom pic-no is_solved-all";
-                                }
-                                if($solved_type == "solved")
-                                {
-                                    echo "time-custom pic-no is_solved-yes";
-                                }
-                                if($solved_type == "not_solved")
-                                {
-                                    echo "time-custom pic-no is_solved-no";
-                                }
-                        }
-                        else
-                        {
-                                if($solved_type == "all")
-                                {
-                                    echo "time-custom pic-yes is_solved-all";
-                                }
-                                if($solved_type == "solved")
-                                {
-                                    echo "time-custom pic-yes is_solved-yes";
-                                }
-                                if($solved_type == "not_solved")
-                                {
-                                    echo "time-custom pic-yes is_solved-no";
-                                }
-                        }
-                }
+                        return 0;
+                
             }
         }
  ?>
