ef279fe000cf717d926792515f6645bee7a1a124
Mithila <mithilamadusanka97@gmail.com>
2020-12-07 12:05:11 +0530

    updated db

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'abhaya',
+	'hostname' => '10.123.0.78',
+	'username' => 'mithmad_abhaya',
+	'password' => 'prHZ1Dg1u3',
+	'database' => 'mithmad_abhaya',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
