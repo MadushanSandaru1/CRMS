d24b7cbbdf87467bdae7b6750c2d29d84be91aa4
Msandaru1 <47908590+Msandaru1@users.noreply.github.com>
2020-12-07 15:00:27 +0530

    guarantor report updated

--- application/views/guarantor_report.php
+++ application/views/guarantor_report.php
@@ -1,13 +1,62 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' integrity='sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2' crossorigin='anonymous'>
+
+    <style>
+        body {
+            width:100%;
+        }
+        table {
+            margin: 0 auto;
+            width: 80%;
+        }
+    </style>
+
 </head>
     <body>
     <img src=assets/images/report_header.png  width=100% heighr=20%>
 
-    <h2 class="text-danger">Heading ඇපකරු</h2>
-    <button class="btn btn-danger">fgfjhfjh</button>
+    <table>
+        <tr>
+            <td colspan="2" style="padding: 0 5px 0 0; color:#a30a36;"><center><h2>Guarantor's Statement</h2></center></td>
+        </tr>
+        <tr>
+            <td style="padding: 10px; width: 30%;">Vehicle Reserved ID</td>
+            <td>: <b>0001</b></td>
+        </tr>
+        <tr>
+            <td style="padding: 10px;">Name</td>
+            <td>: <b>Weeraddana</b></td>
+        </tr>
+        <tr>
+            <td style="padding: 10px;">NIC Number</td>
+            <td>: <b>98256342V</b></td>
+        </tr>
+        <tr>
+            <td style="padding: 10px;">Address</td>
+            <td>: <b>Kolonnawa</b></td>
+        </tr>
+        <tr>
+            <td style="padding: 10px;">Phone Number</td>
+            <td>: <b>0152425369</b></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="padding: 30px 10px 30px 10px;"><p align="justify">I know Mr. / Mrs. <b>Dulu</b> very well who received the car / motorcycle bearing number <b>SP KK2552</b> from Abhaya rent a car on <b>15-10-2020</b> and I will represent those who took over the vehicle in case of breach of terms and conditions of the company. I hereby certify that I am liable on behalf of the person who took over this vehicle, if necessary, for any legal purpose.</p></td>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            <td align="center" style="width: 25%;">___________________</td>
+            <td></td>
+            <td align="center" style="width: 25%;">___________________</td>
+        </tr>
+        <tr>
+            <td align="center" style="width: 25%;">Date</td>
+            <td></td>
+            <td align="center" style="width: 25%;">Signature</td>
+        </tr>
+    </table>
 
 
     </body>
