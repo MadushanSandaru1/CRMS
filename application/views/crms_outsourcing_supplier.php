adaa012d3948230908c90959efb9df6a2691451f
Mithila <mithilamadusanka97@gmail.com>
2021-01-24 12:17:42 +0530

    updated

--- application/views/crms_outsourcing_supplier.php
+++ application/views/crms_outsourcing_supplier.php
@@ -36,7 +36,7 @@ if (!$this->session->userdata('user_id')) {
         </nav>
     </div>
 
-<<<<<<< Updated upstream
+
     <div class="row">
         <div class="col-12 grid-margin stretch-card" id="add_outsource_supplier">
             <div class="card">
@@ -90,18 +90,7 @@ if (!$this->session->userdata('user_id')) {
                                 <span class="input-group-append">
                                         <button class="file-upload-browse btn btn-gradient-primary" type="button">Upload</button>
                                     </span>
-=======
-        <div class="row">
-            <div class="col-12 grid-margin stretch-card" id="add_outsource_supplier">
-                <div class="card">
-                    <div class="card-body">
-                        <?php
-                        if($this->session->flashdata('outsource_supplier_status'))
-                        {
-                            ?>
-                            <div class="alert alert-success">
-                                <?php echo $this->session->flashdata('outsource_supplier_status'); ?>
->>>>>>> Stashed changes
+
                             </div>
                             <small class="text-danger"><?php echo form_error('nic_copy'); ?></small>
                         </div>
@@ -112,7 +101,8 @@ if (!$this->session->userdata('user_id')) {
                 </div>
             </div>
         </div>
-        <div class="col-12 grid-margin stretch-card" id="update_outsource_supplier">
+
+         <div class="col-12 grid-margin stretch-card" id="update_outsource_supplier">
             <div class="card">
                 <div class="card-body">
                     <?php
@@ -135,40 +125,33 @@ if (!$this->session->userdata('user_id')) {
                         </div>
                         <div class="form-group">
                             <label for="expenseVehicleID"><b>Name</b></label>
-                            <input type="text" name="sup_name" class="form-control" placeholder="Type Supplier Name" id="sup_name" value="<?php if($this->session->tempdata('sup_name_fill')) echo $this->session->tempdata('sup_name_fill'); ?>">
+                            <input type="text" name="sup_name" class="form-control" placeholder="Type Supplier Name" id="sup_name">
                             <small class="text-danger"><?php echo form_error('name'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="expensedVehicleDate">NIC</label>
-                            <input type="text" name="sup_nic" class="form-control" id="sup_nic" maxlength="12" pattern="[0-9]{9}[v|V|x|X]|[0-9]{12}" title="Please enter a according to correct pattern." placeholder="Supplier NIC Number" value="<?php if($this->session->tempdata('sup_nic_fill')) echo $this->session->tempdata('sup_nic_fill'); ?>">
+                            <input type="text" name="sup_nic" class="form-control" id="sup_nic" placeholder="Supplier NIC Number" >
                             <small class="text-danger"><?php echo form_error('nic'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="expenseAmount">Email</label>
-                            <input type="email" name="sup_email" class="form-control" id="sup_email" placeholder="Supplier Email" value="<?php if($this->session->tempdata('sup_email_fill')) echo $this->session->tempdata('sup_email_fill'); ?>">
+                            <input type="email" name="sup_email" class="form-control" id="sup_email" placeholder="Supplier Email" >
                             <small class="text-danger"><?php echo form_error('email'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="expenseAmount">Phone Number</label>
-                            <input type="text" name="sup_phone" class="form-control" id="sup_phone" placeholder="Supplier Phone Number" maxlength="10" pattern="0[0-9]{9}" title="Please follow the requested pattern" value="<?php if($this->session->tempdata('sup_phone_fill')) echo $this->session->tempdata('sup_phone_fill'); ?>">
+                            <input type="text" name="sup_phone" class="form-control" id="sup_phone" placeholder="Supplier Phone Number">
                             <small class="text-danger"><?php echo form_error('phone'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="expenseAmount">Address</label>
-                            <input type="text" name="sup_address" class="form-control" id="sup_address" placeholder="Supplier Address" value="<?php if($this->session->tempdata('sup_address_fill')) echo $this->session->tempdata('sup_address_fill'); ?>">
+                            <input type="text" name="sup_address" class="form-control" id="sup_address" placeholder="Supplier Address" >
                             <small class="text-danger"><?php echo form_error('address'); ?></small>
                         </div>
-
                         <div class="form-group">
                             <label for="expenseAmount">Upload NIC Picture</label>
-                            <input type="file" name="sup_nic_copy" class="file-upload-default">
-                            <div class="input-group col-xs-12">
-                                <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-                                <span class="input-group-append">
-                                        <button class="file-upload-browse btn btn-gradient-primary" type="button">Upload</button>
-                                    </span>
-                            </div>
-                            <small class="text-danger"><?php echo form_error('sup_nic_copy'); ?></small>
+                            <input type="file" name="sup_nic_copy" class="form-control" id="expenseAmount" placeholder="Supplier NIC Picture" >
+                            <small class="text-danger"><?php echo form_error('nic_copy'); ?></small>
                         </div>
                         <button type="submit" class="btn btn-gradient-primary mr-2">Update</button>
                         <button class="btn btn-light">Cancel</button>
@@ -176,70 +159,9 @@ if (!$this->session->userdata('user_id')) {
                     </div>
                 </div>
             </div>
-<<<<<<< Updated upstream
         </div>
-        <div class="col-lg-12 grid-margin stretch-card">
-=======
-            <div class="col-12 grid-margin stretch-card" id="update_outsource_supplier">
-                <div class="card">
-                    <div class="card-body">
-                        <?php
-                        if($this->session->flashdata('outsource_supplier_status'))
-                        {
-                            ?>
-                            <div class="alert alert-success">
-                                <?php echo $this->session->flashdata('outsource_supplier_status'); ?>
-                            </div>
-                            <?php
-                        }
-                        ?>
-                        <button type="button" class="btn btn-primary mb-2" data-toggle="collapse" href="#updateOutsourcingSupplier" aria-expanded="false" aria-controls="viewDetails">Update Outsourcing Supplier Details</button>
-
-                        <div class="collapse " id="updateOutsourcingSupplier" aria-labelledby="customRadioInline2">
-                            <br><br>
-                            <?php echo form_open_multipart('OutSourceSupplier/updateOutSourcingSupplier'); ?>
-                            <div class="from-group">
-                                <input type="hidden" name="outsource_sup_id" id="outsource_sup_id">
-                            </div>
-                            <div class="form-group">
-                                <label for="expenseVehicleID"><b>Name</b></label>
-                                <input type="text" name="sup_name" class="form-control" placeholder="Type Supplier Name" id="sup_name">
-                                <small class="text-danger"><?php echo form_error('name'); ?></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="expensedVehicleDate">NIC</label>
-                                <input type="text" name="sup_nic" class="form-control" id="sup_nic" placeholder="Supplier NIC Number" >
-                                <small class="text-danger"><?php echo form_error('nic'); ?></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="expenseAmount">Email</label>
-                                <input type="email" name="sup_email" class="form-control" id="sup_email" placeholder="Supplier Email" >
-                                <small class="text-danger"><?php echo form_error('email'); ?></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="expenseAmount">Phone Number</label>
-                                <input type="text" name="sup_phone" class="form-control" id="sup_phone" placeholder="Supplier Phone Number">
-                                <small class="text-danger"><?php echo form_error('phone'); ?></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="expenseAmount">Address</label>
-                                <input type="text" name="sup_address" class="form-control" id="sup_address" placeholder="Supplier Address" >
-                                <small class="text-danger"><?php echo form_error('address'); ?></small>
-                            </div>
-                            <div class="form-group">
-                                <label for="expenseAmount">Upload NIC Picture</label>
-                                <input type="file" name="sup_nic_copy" class="form-control" id="expenseAmount" placeholder="Supplier NIC Picture" >
-                                <small class="text-danger"><?php echo form_error('nic_copy'); ?></small>
-                            </div>
-                            <button type="submit" class="btn btn-gradient-primary mr-2">Update</button>
-                            <button class="btn btn-light">Cancel</button>
-                            <?php echo form_close(); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg-12 grid-margin stretch-card">
->>>>>>> Stashed changes
+
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title text-danger">Vehicle Outsourcing Supplier Details</h4>
@@ -256,29 +178,7 @@ if (!$this->session->userdata('user_id')) {
                             </tr>
                             </thead>
                             <tbody>
-<<<<<<< Updated upstream
-                            <?php foreach($supplier_details as $values):?>
-                                <tr>
-                                    <td><?php echo $values->name;?></td>
-                                    <td><?php echo $values->nic;?></td>
-                                    <td><?php echo $values->email;?></td>
-                                    <td><?php echo $values->phone;?></td>
-                                    <td><?php echo $values->address;?></td>
-                                    <td>
-                                        <a id="view" data-toggle="collapse" href="#updateOutsourcingSupplier" aria-expanded="false"
-                                           aria-controls="viewDetails"><span class="mdi mdi-eyedropper text-success ml-3"
-                                                                             onclick="update_outsource_supplier('<?php echo $values->id;?>',
-                                                                                     '<?php echo $values->name;?>',
-                                                                                     '<?php echo $values->nic;?>',
-                                                                                     '<?php echo $values->email;?>',
-                                                                                     '<?php echo $values->phone;?>',
-                                                                                     '<?php echo $values->address;?>',
-                                                                                     )">Edit</span></a>
-                                        <label class="cursor-pointer" data-toggle="modal" data-target="#deleteModal" onclick="delete_outsource_supplier('<?php echo $values->id; ?>')"> <span class="mdi mdi-close-circle text-danger ml-4"> Remove</span> </label>
-                                    </td>
-                                </tr>
-                            <?php endforeach;?>
-=======
+
                                 <?php foreach($supplier_details as $values):?>
                                     <tr>
                                         <td><?php echo $values->name;?></td>
@@ -300,14 +200,14 @@ if (!$this->session->userdata('user_id')) {
                                         </td>
                                     </tr>
                                 <?php endforeach;?>    
->>>>>>> Stashed changes
+
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
     <?php if(validation_errors()) { ?>
         <script>
             document.getElementById("addOutsourcingSupplier").classList.add("show");
@@ -337,8 +237,10 @@ if (!$this->session->userdata('user_id')) {
                 <?php echo form_close(); ?>
             </div>
         </div>
-<<<<<<< Updated upstream
+
+
     </div>
+    <!-- content-wrapper ends -->
     <script type="text/javascript">
         function delete_outsource_supplier(del_outsources_s_id){
             document.getElementById("deloutsourcesid").value = del_outsources_s_id;
@@ -359,32 +261,6 @@ if (!$this->session->userdata('user_id')) {
 
         }
     </script>
-</div>
-<!-- content-wrapper ends -->
-=======
-        <script type="text/javascript">
-            function delete_outsource_supplier(del_outsources_s_id){
-                document.getElementById("deloutsourcesid").value = del_outsources_s_id;
-            }
-            function update_outsource_supplier(id,name,nic,email,phone,address){
-
-                //display form if clickd edit in view table
-                document.getElementById("add_outsource_supplier").style.display = "none";
-                document.getElementById("update_outsource_supplier").style.display = "block";
-
-                //load data into form
-                document.getElementById("outsource_sup_id").value = id;
-                document.getElementById("sup_name").value = name;
-                document.getElementById("sup_nic").value = nic;
-                document.getElementById("sup_email").value = email;
-                document.getElementById("sup_phone").value = phone;
-                document.getElementById("sup_address").value = address;
-
-            }
-        </script>
-    </div>
-    <!-- content-wrapper ends -->
->>>>>>> Stashed changes
 <?php require_once 'crms_footer.php';?>
 <?php  if ($this->session->tempdata('form')=='add_form') { ?>
     <script>
